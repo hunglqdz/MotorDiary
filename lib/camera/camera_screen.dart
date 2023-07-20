bbe3621863ddec351f39cf15a08a462569e7f4e9
hunglqdz <lequochung10072002@gmail.com>
2023-07-20 11:41:44 +0700

    model problem

--- lib/camera/camera_screen.dart
+++ lib/camera/camera_screen.dart
@@ -1,5 +1,4 @@
-import 'dart:io';
-
+import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_tflite/flutter_tflite.dart';
 import 'package:image_picker/image_picker.dart';
@@ -12,39 +11,65 @@ class CameraScreen extends StatefulWidget {
 }
 
 class _CameraScreenState extends State<CameraScreen> {
-  File? _image;
+  List<CameraDescription>? cameras;
+  CameraController? controller;
+  XFile? imageFile;
 
   @override
   void initState() {
     super.initState();
-
+    initializeCamera();
     loadModel();
   }
 
-  void loadModel() async {
-    await Tflite.loadModel(
+  Future<void> initializeCamera() async {
+    cameras = await availableCameras();
+    controller = CameraController(
+      cameras![0],
+      ResolutionPreset.medium,
+    );
+    await controller?.initialize();
+  }
+
+  Future<void> loadModel() async {
+    String? res = await Tflite.loadModel(
       model: 'assets/model/detect.tflite',
       labels: 'assets/model/labelmap.txt',
     );
+    print(res);
+  }
+
+  Future<void> takePhoto() async {
+    try {
+      final XFile? file =
+          await ImagePicker().pickImage(source: ImageSource.camera);
+      if (file != null) {
+        setState(() {
+          imageFile = file;
+        });
+        runModelOnImage(imageFile!.path);
+      }
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  Future<void> runModelOnImage(String imagePath) async {
+    try {
+      var recognitions = await Tflite.runModelOnImage(
+        path: imagePath,
+      );
+      print(recognitions);
+    } catch (e) {
+      print(e);
+    }
   }
 
   @override
   void dispose() {
+    controller?.dispose();
     Tflite.close();
     super.dispose();
-  }
-
-  Future<void> _takePhoto() async {
-    final picker = ImagePicker();
-    final pickedImage = await picker.pickImage(source: ImageSource.camera);
-
-    setState(() {
-      if (pickedImage != null) {
-        _image = File(pickedImage.path);
-      } else {
-        print('No image selected.');
-      }
-    });
   }
 
   @override
@@ -53,22 +78,30 @@ class _CameraScreenState extends State<CameraScreen> {
       appBar: AppBar(
         title: const Text('Awesome Camera'),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            if (_image != null)
-              Image.file(_image!)
-            else
-              const Text('No image selected.'),
-            const SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: _takePhoto,
-              child: const Text('Take Photo'),
+      body: Column(
+        children: [
+          Expanded(
+            child: Container(
+              child: cameraPreviewWidget(),
             ),
-          ],
-        ),
+          ),
+          const SizedBox(height: 20),
+          ElevatedButton(
+            onPressed: takePhoto,
+            child: const Text('Take Photo'),
+          ),
+        ],
       ),
+    );
+  }
+
+  Widget cameraPreviewWidget() {
+    if (controller == null || !controller!.value.isInitialized) {
+      return Container();
+    }
+    return AspectRatio(
+      aspectRatio: controller!.value.aspectRatio,
+      child: CameraPreview(controller!),
     );
   }
 }
