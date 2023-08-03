347fdab3a4b7c8773553a9bc481511b3bcba67fa
hunglqdz <lequochung10072002@gmail.com>
2023-08-03 17:26:47 +0700

    load model problem

--- lib/camera/camera_screen.dart
+++ lib/camera/camera_screen.dart
@@ -1,113 +1,7 @@
-// import 'package:camera/camera.dart';
-import 'package:flutter/cupertino.dart';
+import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
-
-import '../event/event_screen.dart';
-// import 'package:flutter_tflite/flutter_tflite.dart';
-// import 'package:image_picker/image_picker.dart';
-//
-// class CameraScreen extends StatefulWidget {
-//   const CameraScreen({super.key});
-//
-//   @override
-//   State<CameraScreen> createState() => _CameraScreenState();
-// }
-//
-// class _CameraScreenState extends State<CameraScreen> {
-//   List<CameraDescription>? cameras;
-//   CameraController? controller;
-//   XFile? imageFile;
-//
-//   @override
-//   void initState() {
-//     super.initState();
-//     initializeCamera();
-//     loadModel();
-//   }
-//
-//   Future<void> initializeCamera() async {
-//     cameras = await availableCameras();
-//     controller = CameraController(
-//       cameras![0],
-//       ResolutionPreset.medium,
-//     );
-//     await controller?.initialize();
-//   }
-//
-//   Future<void> loadModel() async {
-//     String? res = await Tflite.loadModel(
-//       model: 'assets/model/detect.tflite',
-//       labels: 'assets/model/labelmap.txt',
-//     );
-//     print(res);
-//   }
-//
-//   Future<void> takePhoto() async {
-//     try {
-//       final XFile? file =
-//           await ImagePicker().pickImage(source: ImageSource.camera);
-//       if (file != null) {
-//         setState(() {
-//           imageFile = file;
-//         });
-//         runModelOnImage(imageFile!.path);
-//       }
-//     } catch (e) {
-//       print(e);
-//     }
-//   }
-//
-//   Future<void> runModelOnImage(String imagePath) async {
-//     try {
-//       var recognitions = await Tflite.runModelOnImage(
-//         path: imagePath,
-//       );
-//       print(recognitions);
-//     } catch (e) {
-//       print(e);
-//     }
-//   }
-//
-//   @override
-//   void dispose() {
-//     controller?.dispose();
-//     Tflite.close();
-//     super.dispose();
-//   }
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: const Text('Awesome Camera'),
-//       ),
-//       body: Column(
-//         children: [
-//           Expanded(
-//             child: Container(
-//               child: cameraPreviewWidget(),
-//             ),
-//           ),
-//           const SizedBox(height: 20),
-//           ElevatedButton(
-//             onPressed: takePhoto,
-//             child: const Text('Take Photo'),
-//           ),
-//         ],
-//       ),
-//     );
-//   }
-//
-//   Widget cameraPreviewWidget() {
-//     if (controller == null || !controller!.value.isInitialized) {
-//       return Container();
-//     }
-//     return AspectRatio(
-//       aspectRatio: controller!.value.aspectRatio,
-//       child: CameraPreview(controller!),
-//     );
-//   }
-// }
+import 'package:flutter_tflite/flutter_tflite.dart';
+import 'package:image_picker/image_picker.dart';
 
 class CameraScreen extends StatefulWidget {
   const CameraScreen({super.key});
@@ -117,21 +11,97 @@ class CameraScreen extends StatefulWidget {
 }
 
 class _CameraScreenState extends State<CameraScreen> {
+  List<CameraDescription>? cameras;
+  CameraController? controller;
+  XFile? imageFile;
+
+  @override
+  void initState() {
+    super.initState();
+    initializeCamera();
+    loadModel();
+  }
+
+  Future<void> initializeCamera() async {
+    cameras = await availableCameras();
+    controller = CameraController(
+      cameras![0],
+      ResolutionPreset.medium,
+    );
+    await controller?.initialize();
+  }
+
+  Future<void> loadModel() async {
+    String? res = await Tflite.loadModel(
+      model: 'assets/model/detect.tflite',
+      labels: 'assets/model/labelmap.txt',
+    );
+    print(res);
+  }
+
+  Future<void> takePhoto() async {
+    try {
+      final XFile? file =
+          await ImagePicker().pickImage(source: ImageSource.camera);
+      if (file != null) {
+        setState(() {
+          imageFile = file;
+        });
+        runModelOnImage(imageFile!.path);
+      }
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  Future<void> runModelOnImage(String imagePath) async {
+    try {
+      var recognitions = await Tflite.runModelOnImage(
+        path: imagePath,
+      );
+      print(recognitions);
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  @override
+  void dispose() {
+    controller?.dispose();
+    Tflite.close();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Awesome Camera'),
-        centerTitle: true,
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () async {
-          await Navigator.push(context,
-              MaterialPageRoute(builder: (context) => const EventScreen()));
-          setState(() {});
-        },
-        child: const Icon(CupertinoIcons.camera),
+      body: Column(
+        children: [
+          Expanded(
+            child: Container(
+              child: cameraPreviewWidget(),
+            ),
+          ),
+          const SizedBox(height: 20),
+          ElevatedButton(
+            onPressed: takePhoto,
+            child: const Text('Take Photo'),
+          ),
+        ],
       ),
+    );
+  }
+
+  Widget cameraPreviewWidget() {
+    if (controller == null || !controller!.value.isInitialized) {
+      return Container();
+    }
+    return AspectRatio(
+      aspectRatio: controller!.value.aspectRatio,
+      child: CameraPreview(controller!),
     );
   }
 }
--- lib/pages/timeline.dart
+++ lib/pages/timeline.dart
@@ -1,10 +1,5 @@
 import 'package:flutter/material.dart';
 
-import '../event/event_model.dart';
-import '../event/event_screen.dart';
-import '../service/database_helper.dart';
-import '../widget/event_widget.dart';
-
 class TimelinePage extends StatefulWidget {
   const TimelinePage({super.key});
 
@@ -16,72 +11,11 @@ class _TimelinePageState extends State<TimelinePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text('Event list'),
-          centerTitle: true,
-        ),
-        body: FutureBuilder<List<Event>?>(
-          future: DatabaseHelper.getAllEvents(),
-          builder: (context, AsyncSnapshot<List<Event>?> snapshot) {
-            if (snapshot.connectionState == ConnectionState.waiting) {
-              return const CircularProgressIndicator();
-            } else if (snapshot.hasError) {
-              return Center(
-                child: Text(snapshot.error.toString()),
-              );
-            } else if (snapshot.hasData) {
-              if (snapshot.data != null) {
-                return ListView.builder(
-                  itemBuilder: (context, index) => EventWidget(
-                    event: snapshot.data![index],
-                    onTap: () async {
-                      await Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) => EventScreen(
-                                    event: snapshot.data![index],
-                                  )));
-                      setState(() {});
-                    },
-                    onLongPress: () async {
-                      showDialog(
-                          context: context,
-                          builder: (context) {
-                            return AlertDialog(
-                              title:
-                                  const Text('You want to delete this event?'),
-                              actions: [
-                                ElevatedButton(
-                                  style: ButtonStyle(
-                                      backgroundColor:
-                                          MaterialStateProperty.all(
-                                              Colors.red)),
-                                  onPressed: () async {
-                                    await DatabaseHelper.deleteEvent(
-                                        snapshot.data![index]);
-                                    Navigator.pop(context);
-                                    setState(() {});
-                                  },
-                                  child: const Text('Yes'),
-                                ),
-                                ElevatedButton(
-                                  onPressed: () => Navigator.pop(context),
-                                  child: const Text('No'),
-                                ),
-                              ],
-                            );
-                          });
-                    },
-                  ),
-                  itemCount: snapshot.data!.length,
-                );
-              }
-              return const Center(
-                child: Text('No events yet'),
-              );
-            }
-            return const SizedBox.shrink();
-          },
-        ));
+      appBar: AppBar(
+        title: const Text('Event list'),
+        centerTitle: true,
+      ),
+      body: Container(),
+    );
   }
 }
